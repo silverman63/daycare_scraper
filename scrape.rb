1c562ff4e42c84d78dfb739361a39447bf9d7493
Alex Silverman <alex@silverswatch.com>
2015-12-07 21:45:38 +0000

    a few more fixes. now creates giant array of all daycares

--- scrape.rb
+++ scrape.rb
@@ -31,19 +31,16 @@ form = page.forms.first
 page = form.submit
 agent1.cookie_jar.save_as 'cookies', :session => true, :format => :yaml
 
+daycares = []
+
 while offset < 2300 do #  < 10 do for testing
 	puts "Offset: " + offset.to_s
 	page = agent1.post 'https://a816-healthpsi.nyc.gov/ChildCare/SearchAction2.do?pager.offset=' + offset.to_s, form_data
 
-	# text = page.body
-	# puts "does text contain pickle?"
-	# puts text.include?('PICKLE')
-	# puts 'endcheck'
-
 	links = page.search('tr.gradeX.odd a')
 
 	links.each do |link|
-		
+
 		id = link.to_s()
 		id = id.split('redirectHistory(')[1]
 		id = id[0,10]
@@ -57,41 +54,43 @@ while offset < 2300 do #  < 10 do for testing
 
 		page2 = agent2.post 'https://a816-healthpsi.nyc.gov/ChildCare/WDetail.do', idString ,({'Content-Type' => 'application/x-www-form-urlencoded'})
 
-		data = {}
+		daycare = {}
 
 		infoBoxes = page2.search('.projectBox')
 
 		firstbox = infoBoxes[0]
 		firstboxData = firstbox.search('h5')
-		data["type"] = firstboxData[0].text
-		data["centerName"] = firstboxData[1].text
-		data["permitHolder"] = firstboxData[2].text
-		data["address"] = firstboxData[3].text
+		daycare["type"] = firstboxData[0].text
+		daycare["centerName"] = firstboxData[1].text
+		daycare["permitHolder"] = firstboxData[2].text
+		daycare["address"] = firstboxData[3].text
 		address2 = firstboxData[4].text.split(',')
-		data['borough'] = address2[0]
-		data['zipCode'] = address2[1].split(" ")[-1]
-		data['phone'] = firstboxData[5].text
+		daycare['borough'] = address2[0]
+		daycare['zipCode'] = address2[1].split(" ")[-1]
+		daycare['phone'] = firstboxData[5].text
 
 		morebox = infoBoxes[1]
 		moreboxData = morebox.search('tr')
-		data['permitStatus'] = moreboxData[0].search('h5')[1].text
-		data['permitNumber'] = moreboxData[1].search('h5')[1].text
-		data['permitExpirationDate'] = moreboxData[2].search('h5')[1].text
-		data['ageRange'] = moreboxData[3].search('h5')[1].text
-		data['maximumCapacity'] = moreboxData[4].search('h5')[1].text
-		data['siteType'] = moreboxData[5].search('h5')[1].text
-		data['certrm ifiedToAdministerMedication'] = moreboxData[6].search('h5')[1].text
-		data['yearsOperating'] = moreboxData[7].search('h5')[1].text
+		daycare['permitStatus'] = moreboxData[0].search('h5')[1].text
+		daycare['permitNumber'] = moreboxData[1].search('h5')[1].text
+		daycare['permitExpirationDate'] = moreboxData[2].search('h5')[1].text
+		daycare['ageRange'] = moreboxData[3].search('h5')[1].text
+		daycare['maximumCapacity'] = moreboxData[4].search('h5')[1].text
+		daycare['siteType'] = moreboxData[5].search('h5')[1].text
+		daycare['certrm ifiedToAdministerMedication'] = moreboxData[6].search('h5')[1].text
+		daycare['yearsOperating'] = moreboxData[7].search('h5')[1].text
 
 		
-		# cleaning regex from old version?
-		data.each do |datum|
+		daycare.each do |datum|
+			# cleaning regex from old version
 			datum[1].gsub(/[^\w\s\-\/]/, "").gsub(/\r?\n|\r/,"")
+			puts datum[0] + ": " + datum[1]
 			# output.print(datum[1])
 			# output.print(",")
 		end
 
 		#output.print("\n")		
+		daycares.push(daycare)
 
 	end
 
