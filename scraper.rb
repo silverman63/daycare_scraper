7b46ee7a5b3fb9d66bda19136f32831afa6a786f
Alex Silverman <alex@silverswatch.com>
2015-12-08 21:21:12 +0000

    moved specifics for pagescraping to own file, adding scraping of most recent inspection and corresponding violations

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'mechanize'
 require 'csv'
+require_relative 'pagescrape.rb'
 
 agent1 = Mechanize.new
 agent2 = Mechanize.new
@@ -33,7 +34,7 @@ agent1.cookie_jar.save_as 'cookies', :session => true, :format => :yaml
 
 daycares = []
 
-while offset < 2300 do #  < 10 do for testing
+while offset < 2300 do #  < 10 for testing, 2300 at least for real
 	puts "Offset: " + offset.to_s
 	page = agent1.post 'https://a816-healthpsi.nyc.gov/ChildCare/SearchAction2.do?pager.offset=' + offset.to_s, form_data
 
@@ -53,43 +54,7 @@ while offset < 2300 do #  < 10 do for testing
 		# puts idString
 
 		page2 = agent2.post 'https://a816-healthpsi.nyc.gov/ChildCare/WDetail.do', idString ,({'Content-Type' => 'application/x-www-form-urlencoded'})
-
-		daycare = {}
-
-		infoBoxes = page2.search('.projectBox')
-
-		firstbox = infoBoxes[0]
-		firstboxData = firstbox.search('h5')
-		daycare["type"] = firstboxData[0].text
-		daycare["centerName"] = firstboxData[1].text
-		daycare["permitHolder"] = firstboxData[2].text
-		daycare["address"] = firstboxData[3].text
-		address2 = firstboxData[4].text.split(',')
-		daycare['borough'] = address2[0]
-		daycare['zipCode'] = address2[1].split(" ")[-1]
-		daycare['phone'] = firstboxData[5].text
-
-		morebox = infoBoxes[1]
-		moreboxData = morebox.search('tr')
-		daycare['permitStatus'] = moreboxData[0].search('h5')[1].text
-		daycare['permitNumber'] = moreboxData[1].search('h5')[1].text
-		daycare['permitExpirationDate'] = moreboxData[2].search('h5')[1].text
-		daycare['ageRange'] = moreboxData[3].search('h5')[1].text
-		daycare['maximumCapacity'] = moreboxData[4].search('h5')[1].text
-		daycare['siteType'] = moreboxData[5].search('h5')[1].text
-		daycare['certrm ifiedToAdministerMedication'] = moreboxData[6].search('h5')[1].text
-		daycare['yearsOperating'] = moreboxData[7].search('h5')[1].text
-
-		
-		daycare.each do |datum|
-			# cleaning regex from old version
-			datum[1].gsub(/[^\w\s\-\/]/, "").gsub(/\r?\n|\r/,"")
-			puts datum[0] + ": " + datum[1]
-			# output.print(datum[1])
-			# output.print(",")
-		end
-
-		#output.print("\n")		
+		daycare = pagescrape(page2)	
 		daycares.push(daycare)
 
 	end
@@ -97,5 +62,3 @@ while offset < 2300 do #  < 10 do for testing
 offset = offset + 10
 
 end
-
-
